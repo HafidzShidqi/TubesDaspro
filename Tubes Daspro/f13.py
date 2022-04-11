8c25f55e0809d0772497878055eeac5f4cf95871
nnisaf <103409268+nnisaf@users.noreply.github.com>
2022-04-11 14:27:11 +0700

    Add files via upload

---
+++ Tubes Daspro/f13.py
@@ -0,0 +1,8 @@
+from konversiCSVTArray import *
+from fungsi import *
+
+def riwayat ():
+    if dataRiwayat != []:
+        printRapih(dataRiwayat)
+    else:
+        print("Maaf, kamu tidak ada riwayat pembelian. Ketik perintah beli_game untuk membeli.")
