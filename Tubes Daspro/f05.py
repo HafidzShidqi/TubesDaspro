6ebbcd6e7b490813a56d52484ecfca65d7ab6792
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2022-04-19 08:22:29 +0700

    Update f05.py

--- Tubes Daspro/f05.py
+++ Tubes Daspro/f05.py
@@ -7,11 +7,11 @@ def ubah_game(dataGame):
     kategori = input("Masukkan kategori: ")
     tahun_rilis = input("Masukkan tahun rilis: ")
     harga = input("Masukkan harga: ")
-    foundID = False
+    foundID = False     #inisialisasi
     for i in range(Length(dataGame)):
-        if dataGame[i][0] == id:
+        if dataGame[i][0] == id:        #cek id
             foundID = True
-            if nama != '':
+            if nama != '':              #jika input "" maka datanya akan sama dengan data awal
                 dataGame[i][1] = nama
             if kategori != '':
                 dataGame[i][2] = kategori
@@ -20,9 +20,9 @@ def ubah_game(dataGame):
             if harga != '':
                 dataGame[i][4] = harga
 
-    if foundID == False:
+    if foundID == False: #jika id tidak ada di data
         print("\nTidak ada game dengan ID tersebut!\n")
-    else:
+    else: #jika id ada di data
         print("\nBerhasil mengubah game!\n")
     
     return dataGame
