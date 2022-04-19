842500df2a828383f8f67733593a631d9f9a4b55
HafidzShidqi <88760556+HafidzShidqi@users.noreply.github.com>
2022-04-19 07:59:27 +0700

    Delete test.py

--- Tubes Daspro/test.py
+++
@@ -1,7 +0,0 @@
-username = input("ID : ")
-
-for i in username :
-    if ( (ord(i) == 45) or (47 < ord(i) < 58) or (64 < ord(i) < 91) or (ord(i) == 95) or (96 < ord(i) < 123)):
-        print(f"{i} = True")
-    else:
-        print(f"{i} = False")
