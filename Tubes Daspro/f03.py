62e41fe07e044b0e0e8ff2afa0b8a67024248ffd
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2022-04-19 08:17:51 +0700

    Update f03.py

--- Tubes Daspro/f03.py
+++ Tubes Daspro/f03.py
@@ -6,19 +6,19 @@ def login(dataUser):
     password = input("Masukan password: ")
     
     j = 0
-    valid = False
+    valid = False       #inisialisasi
     for i in range(1,Length(dataUser)):
         if dataUser[i][1] == username and dataUser[i][3] == password:
             valid = True
             j = i
     
-
-    if valid == False:
+    
+    if valid == False:     #jika username dan password tidak sesuai   
         print("\nPassword atau username salah atau tidak ditemukan\n")
-        return login(dataUser)
-    else:
-        print(f'\nHalo {dataUser[j][2]}! Selamat datang di "Binomo".\n')
-        userid = dataUser[j][0]
+        return login(dataUser)      #memanggil fungsi login lagi
+    else: #jika username dan password sesuai
+        print(f'\nHalo {dataUser[j][2]}! Selamat datang di "Binomo".\n')    
+        userid = dataUser[j][0]     
         return dataUser[j][4]
 
 
