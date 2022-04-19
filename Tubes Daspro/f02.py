022bfd57f4746cd82f1d11a556f268705abb7eb7
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2022-04-19 08:13:32 +0700

    Update f02.py

--- Tubes Daspro/f02.py
+++ Tubes Daspro/f02.py
@@ -5,7 +5,7 @@ from fungsi import *
 def isUsernameValid(username):
     nonValid = 0
     for i in username:
-        if ( (ord(i) == 45) or (47 < ord(i) < 58) or (64 < ord(i) < 91) or (ord(i) == 95) or (96 < ord(i) < 123)):
+        if ( (ord(i) == 45) or (47 < ord(i) < 58) or (64 < ord(i) < 91) or (ord(i) == 95) or (96 < ord(i) < 123)):  
             continue
         else:
             nonValid += 1
@@ -27,16 +27,16 @@ def register(dataUser):
         if str(dataUser[i][1]) == str(username):
             terpakai = True
 
-    if isUsernameValid(username) == False:
+    if isUsernameValid(username) == False:  #karakter dari username ada yang tidak sesuai dengan ketentuan            
         print('Username hanya dapat mengandung alfabet A-Z, a-z, underscore “_”, strip “-”, dan angka 0-9\n')
-    elif terpakai == False:
+    elif terpakai == False:     #username memenuhi ketentuan dan belum digunakan oleh user lain
         id = str(Length(dataUser))
         for i in range(3-Length(id)): 
             id = append('0', id)    
         id = append('USER', id) 
         dataUser = append(dataUser, [[id,username, nama, password, "User", "0"]])
         print(f'\nUsername {username} telah berhasil register ke dalam "Binomo"\n')
-    else:
+    else: #username sudah digunakan oleh user lain
         print(f'\nUsername {username} sudah terpakai, silakan menggunakan username lain\n')
     
     return dataUser
